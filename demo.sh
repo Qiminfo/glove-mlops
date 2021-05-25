901b1d4b8c67140877531d0c493abf7e70f4d085
Julien RUEY <julien.ruey@qiminfo.ch>
2021-05-25 12:05:41 +0200

    fixed demo.sh

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
-valohai-local-run load --output-path "../data" && \
-valohai-local-run transform --data-file ../data/demo_data_train.json --output-path ../data/cleansed_data.json --adhoc && \
-valohai-local-run cooccurrence --cleansed-cvs "../data/cleansed_data.json" --output-path "../data" && \
-python3 src/main.py train_glove --counter "../data/counter.pkl" --cooccurrence-matrix "../data/cooccurrence_matrix.npz" --output-path "../data/glove_embedder.pkl" && \
-python3 src/main.py apply --glove-embedder "../data/glove_embedder.pkl" --labeled-docs-path "../data/demo_data_test.json" --output-path "../data/embedded_data.json" && \
-python3 src/main.py evaluate --embedded-cvs-labeled "../data/embedded_data.json" --labeled-docs-path "../data/demo_data_test.json"
+python3 src/main.py load_data --output-path "./resources" && \
+python3 src/main.py clean_text --data-file "./resources/demo_data_train.json" --output-path "./resources/cleansed_data.json" && \
+python3 src/main.py build_cooccurrence --cleansed-cvs "./resources/cleansed_data.json" --output-path "./resources" && \
+python3 src/main.py train_glove --counter "./resources/counter.pkl" --cooccurrence-matrix "./resources/cooccurrence_matrix.npz" --output-path "./resources/glove_embedder.pkl" && \
+python3 src/main.py apply --glove-embedder "./resources/glove_embedder.pkl" --labeled-docs-path "./resources/demo_data_test.json" --output-path "./resources/embedded_data.json" && \
+python3 src/main.py evaluate --embedded-cvs-labeled "./resources/embedded_data.json" --labeled-docs-path "./resources/demo_data_test.json"
